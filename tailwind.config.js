6418fdf658f73a52889f190b2f75ba13de338c60
Jason Lee <122265418+jasonleex@users.noreply.github.com>
2023-01-09 11:54:28 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: ['./public/**/*.html'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        lightGrayishBlue: '#fbfcfd'
+      },
+      boxShadow: {
+        xs: '0 0 0 1px rgba(0, 0, 0, 0.16)',
+        sm: '0 1px 2px 0 rgba(0, 0, 0, 0.16)',
+        default:
+          '0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.03)',
+        md:
+          '0 4px 6px -1px rgba(0, 0, 0, 0.04), 0 2px 4px -1px rgba(0, 0, 0, 0.03)',
+        lg:
+          '0 10px 15px -3px rgba(0, 0, 0, 0.04), 0 4px 6px -2px rgba(0, 0, 0, 0.02)',
+        xl:
+          '0 20px 25px -5px rgba(0, 0, 0, 0.12), 0 10px 10px -5px rgba(0, 0, 0, 0.02)',
+        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.15)',
+        inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.04)',
+        outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+        none: 'none'
+      }
+    }
+  },
+  variants: {
+    boxShadow: ['responsive', 'hover', 'focus', 'focus-within']
+  },
+  plugins: []
+};
